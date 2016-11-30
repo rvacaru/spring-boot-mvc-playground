50784132735262dbc98c98754b741b695440a954
Razvan Vacaru <r.vacaru@relay42.com>
2016-11-30 16:16:47 +0100

    Add Spring test annotations on CustomerControllerTest and removed unused test class

--- src/test/java/com/raz/controller/CustomerControllerTest.java
+++ src/test/java/com/raz/controller/CustomerControllerTest.java
@@ -4,9 +4,12 @@ import com.raz.domain.Customer;
 import com.raz.service.CustomerService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -27,6 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CustomerControllerTest {
 
 	@Mock
